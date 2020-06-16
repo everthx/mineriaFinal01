999c67d32e1a75ac6432f823c2a22a86dc410933
EverthX <fernando.luna16@tectijuana.edu.mx>
2020-06-15 20:37:35 -0700

    se crea def deparat_columnas(), se elimina el SpellCheckingInspetion en el IDE, se agrega PENDIENTE para filtrar_ciudades()

--- datafrane.py
+++ datafrane.py
@@ -1,15 +1,19 @@
 import pandas as pd
+
+
+# noinspection SpellCheckingInspection
 class Dataframe():
 
     pd.set_option('display.width', 300)
-    pd.set_option('display.max_columns', 50)
+    pd.set_option('display.max_columns', None)
     df = pd.read_csv('01_POBLACION_01_BC.csv', encoding="ISO-8859-1")
 
-    #mod 123
+    # mod 123
     def filtrar_dataset(df):
-        df = df.rename( columns = { 'INEGI. Tabulados de la Encuesta Intercensal 2015': 'Estado'
-            ,'Unnamed: 1': 'Dimension', 'Unnamed: 2': 'Edades', 'Unnamed: 3': 'Estimador', 'Unnamed: 4': 'Poblacion Total'
-            ,'Unnamed: 5':'Hombres', 'Unnamed: 6': 'Mujeres' } )
+        df = df.rename(columns={'INEGI. Tabulados de la Encuesta Intercensal 2015': 'Estado'
+            , 'Unnamed: 1': 'Dimension', 'Unnamed: 2': 'Edades', 'Unnamed: 3': 'Estimador',
+                                'Unnamed: 4': 'Poblacion Total'
+            , 'Unnamed: 5': 'Hombres', 'Unnamed: 6': 'Mujeres'})
         df = df.loc[7: 654].reset_index(drop=True)
         df = df.loc[df['Estimador'] == 'Valor'].reset_index(drop=True)
 
@@ -25,7 +29,7 @@ class Dataframe():
         df.to_csv('poblacion_filtrado.csv', index=False)
         df = pd.read_csv('poblacion_filtrado.csv')
 
-        #df['Total 2'] = df.iloc[:, 5:7].sum(axis=1)
+        # df['Total 2'] = df.iloc[:, 5:7].sum(axis=1)
         return df
 
     def filtrar_ciudades(df):
@@ -35,23 +39,47 @@ class Dataframe():
         df['Dimension'] = df['Dimension'].str.replace('15 000-49 999 habitantes', 'Ensenada')
         df['Dimension'] = df['Dimension'].str.replace('50 000-99 999 habitantes', 'Mexicali')
         df['Dimension'] = df['Dimension'].str.replace('100 000 y más habitantes', 'Tijuana')
+
+        # << FALTA AGREGAR>> Cambio a categorias numericas los rangos de edades
+
         df.to_csv('poblacion_ciudades.csv', index=False)
         df = pd.read_csv('poblacion_ciudades.csv')
         return df
 
+    def separar_columnas(df):
+        #       Declaracion de nuestras columnas para el plot
+        x, y1, y2 = (df['Dimension'], df['Hombres'], df['Mujeres'])
+        return x, y1, y2
+
     nuevoDataset = filtrar_dataset(df)
     nuevoDataset = filtrar_ciudades(nuevoDataset)
+    x, y1, y2 = separar_columnas(nuevoDataset)
 
-    #       Declaracion de nuestras columnas para el plot
-    x = nuevoDataset['Dimension']
-    y1 = nuevoDataset['Hombres']
-    y2 = nuevoDataset['Mujeres']
-    #       Vista de los 3
-    for values in zip(x,y1,y2):
-        print(values)
+    #print( nuevoDataset['Edades'].head(20) )
 
 
-    #test = nuevoDataset.groupby('Dimension').describe()
-    #test.to_csv('purbas.csv')
-    #print(nuevoDataset.groupby('Dimension').max())
-    #print(nuevoDataset.chunksize())
+
+
+
+
+
+    #   ================================================== Prubas de codigo e ideas ==================================================
+    #   ===============================================================================================================================
+    """
+    def tester(equipo):
+        a,s,d = (equipo[0],equipo[1], equipo[2])
+        return a,s,d
+
+    team = ['Adrian','Gabriel','Jesus']
+    a,s,d = tester(team)
+    print(a,s,d)
+    """
+
+    #       ver las 3 listas de columnas
+    #for values in zip(x, y1, y2):
+    #    print(values)
+
+    # test = nuevoDataset.groupby('Dimension').describe()
+    # test.to_csv('purbas.csv')
+    # print(nuevoDataset.groupby('Dimension').max())
+    # print(nuevoDataset.chunksize())
